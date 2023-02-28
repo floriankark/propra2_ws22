b8b36e70d0c5fe2c94c787937bb7c857ec98e9c2
floriankark <florian.kark@gmail.com>
2023-02-28 13:45:05 +0100

    add thymeleaf

--- BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/Widmark.java
+++ BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/Widmark.java
@@ -2,8 +2,15 @@ package com.example.blutalkoholwertberechner;
 
 import org.springframework.web.bind.annotation.RequestParam;
 
-public record Widmark(@RequestParam("geschlecht") String geschlecht, int gewicht, int bier, int wein, int korn, int vodka, int whiskey) {
-
+public record Widmark(
+        @RequestParam("geschlecht") String geschlecht,
+        int gewicht,
+        int bier,
+        int wein,
+        int korn,
+        int vodka,
+        int whiskey
+) {
     public double getWidmark(){
         double A = (bier*0.05 + wein*0.12 + korn*0.32 + vodka*0.4 + whiskey*0.43)*0.8;
         double r = geschlecht.equals("m") ? 0.7 : 0.6;
--- BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/widmarkController.java
+++ BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/widmarkController.java
@@ -1,14 +1,26 @@
 package com.example.blutalkoholwertberechner;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class widmarkController {
-    @GetMapping("berechnen")
-    public @ResponseBody
-    double widmark(Widmark w){
-        return w.getWidmark();
+    @GetMapping("/")
+    public String index(Model m){
+        m.addAttribute("ergebnis", null);
+        m.addAttribute("hatErgebnis", false);
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String widmark(Widmark w, Model m){
+        m.addAttribute("ergebnis", w.getWidmark());
+        m.addAttribute("hatErgebnis", true);
+        return "index";
     }
 }
