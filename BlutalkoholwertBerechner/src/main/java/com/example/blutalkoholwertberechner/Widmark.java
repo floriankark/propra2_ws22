c3ef61092a218f5eeca9a90d4e23333225b9328e
floriankark <florian.kark@gmail.com>
2023-02-28 18:59:06 +0100

    add design and validation

--- BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/Widmark.java
+++ BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/Widmark.java
@@ -1,15 +1,16 @@
 package com.example.blutalkoholwertberechner;
 
+import jakarta.validation.constraints.Min;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public record Widmark(
         @RequestParam("geschlecht") String geschlecht,
-        int gewicht,
-        int bier,
-        int wein,
-        int korn,
-        int vodka,
-        int whiskey
+        @Min(value = 30, message = "Sie müssen min. 30 Kg wiegen") int gewicht,
+        @Min(value = 0, message = "Negative Werte sind nicht erlaubt!") int bier,
+        @Min(value = 0, message = "Negative Werte sind nicht erlaubt!") int wein,
+        @Min(value = 0, message = "Negative Werte sind nicht erlaubt!") int korn,
+        @Min(value = 0, message = "Negative Werte sind nicht erlaubt!") int vodka,
+        @Min(value = 0, message = "Negative Werte sind nicht erlaubt!") int whiskey
 ) {
     public double getWidmark(){
         double A = (bier*0.05 + wein*0.12 + korn*0.32 + vodka*0.4 + whiskey*0.43)*0.8;
--- BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/widmarkController.java
+++ BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/widmarkController.java
@@ -1,7 +1,9 @@
 package com.example.blutalkoholwertberechner;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +14,19 @@ import org.springframework.web.servlet.view.RedirectView;
 public class widmarkController {
     @GetMapping("/")
     public String index(Model m){
+        m.addAttribute("w", null);
         m.addAttribute("ergebnis", null);
         m.addAttribute("hatErgebnis", false);
         return "index";
     }
 
     @PostMapping("/")
-    public String widmark(Widmark w, Model m){
-        m.addAttribute("ergebnis", w.getWidmark());
+    public String widmark(@Valid Widmark w, BindingResult bindingResult, Model m){
+        if (bindingResult.hasErrors()) {
+            return "index";
+        }
+        m.addAttribute("w", w);
+        m.addAttribute("ergebnis", String.format("%.2f", w.getWidmark()));
         m.addAttribute("hatErgebnis", true);
         return "index";
     }
