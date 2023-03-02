a359ec1d4d54ba7eb928686d77798c64bacb316e
floriankark <florian.kark@gmail.com>
2023-03-02 18:15:15 +0100

    add validation

--- BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/widmarkController.java
+++ BlutalkoholwertBerechner/src/main/java/com/example/blutalkoholwertberechner/widmarkController.java
@@ -4,25 +4,25 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class widmarkController {
     @GetMapping("/")
     public String index(Model m){
-        m.addAttribute("w", null);
+        m.addAttribute("w", new Widmark("m", 0, 0, 0, 0, 0, 0));
         m.addAttribute("ergebnis", null);
         m.addAttribute("hatErgebnis", false);
         return "index";
     }
 
     @PostMapping("/")
-    public String widmark(@Valid Widmark w, BindingResult bindingResult, Model m){
+    public String widmark(@ModelAttribute("w") @Valid Widmark w, BindingResult bindingResult, Model m){
         if (bindingResult.hasErrors()) {
+            m.addAttribute("w", w);
+            m.addAttribute("ergebnis", null);
+            m.addAttribute("hatErgebnis", false);
             return "index";
         }
         m.addAttribute("w", w);
