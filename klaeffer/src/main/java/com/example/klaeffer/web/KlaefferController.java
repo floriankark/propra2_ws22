6e8dd3ad4c57ee447bfb5f1f4b7cb1a728a98c04
floriankark <florian.kark@gmail.com>
2023-03-07 19:16:21 +0100

    add Klaeffer and test

--- klaeffer/src/main/java/com/example/klaeffer/web/KlaefferController.java
+++ klaeffer/src/main/java/com/example/klaeffer/web/KlaefferController.java
@@ -1,14 +1,28 @@
 package com.example.klaeffer.web;
 
+import com.example.klaeffer.service.KlaefferService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class KlaefferController {
 
+    private final KlaefferService service;
+
+    public KlaefferController(KlaefferService service) {
+        this.service = service;
+    }
+
     @GetMapping("/")
-    public String startseite(){
+    public String index(){
         return "index";
     }
 
+    @PostMapping("/")
+    public String addKlaeff(String name, String text){
+        System.out.println(name);
+        service.addKlaeffer(name, text);
+        return "redirect:/";
+    }
 }
--- klaeffer/src/test/java/com/example/klaeffer/web/KlaefferControllerTests.java
+++ klaeffer/src/test/java/com/example/klaeffer/web/KlaefferControllerTests.java
@@ -1,11 +1,16 @@
 package com.example.klaeffer.web;
 
+import com.example.klaeffer.service.KlaefferService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -14,9 +19,24 @@ public class KlaefferControllerTests {
     @Autowired
     MockMvc mvc;
 
+    @MockBean
+    KlaefferService service;
+
     @Test
     @DisplayName("Startseite erreichbar")
     void test01()throws Exception{
         mvc.perform(get("/")).andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Kläff hinzufügen")
+    void test_validation4() throws Exception {
+        String text = "Wilkommen auf Kläffer!";
+        String name = "Flo";
+        mvc.perform(post("/")
+                .param("name", name)
+                .param("text", text))
+                .andExpect(status().is3xxRedirection());
+        verify(service).addKlaeffer(name, text);
     }
 }
