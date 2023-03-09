3b2d2a718623cd61b45f631e37317a42c7e87df8
floriankark <florian.kark@gmail.com>
2023-03-09 14:24:04 +0100

    add automatic name setting from oauth token

--- klaeffer/src/main/java/com/example/klaeffer/web/KlaefferController.java
+++ klaeffer/src/main/java/com/example/klaeffer/web/KlaefferController.java
@@ -2,6 +2,8 @@ package com.example.klaeffer.web;
 
 import com.example.klaeffer.service.KlaefferService;
 import jakarta.validation.Valid;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,17 +21,19 @@ public class KlaefferController {
     }
 
     @GetMapping("/")
-    public String index(KlaefferForm klaefferForm, Model m){
+    public String index(KlaefferForm klaefferForm, Model m, OAuth2AuthenticationToken auth){
+        klaefferForm.setName(auth.getPrincipal().getAttribute("login"));
         m.addAttribute("klaeffer", service.getKlaeffer());
         return "index";
     }
 
     @PostMapping("/")
-    public String addKlaeff(@Valid KlaefferForm klaefferForm, BindingResult bindingResult, RedirectAttributes attrs){
+    public String addKlaeff(@Valid KlaefferForm klaefferForm, BindingResult bindingResult, RedirectAttributes attrs, OAuth2AuthenticationToken auth){
+        klaefferForm.setName(auth.getPrincipal().getAttribute("login"));
         if (bindingResult.hasErrors()){
             return "index";
         }
-        service.addKlaeffer(klaefferForm.getName(), klaefferForm.getText());
+        service.addKlaeffer(auth.getPrincipal().getAttribute("login"), klaefferForm.getText());
         attrs.addFlashAttribute("klaefferForm", klaefferForm);
         return "redirect:/";
     }
